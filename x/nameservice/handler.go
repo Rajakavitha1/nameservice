75584a44432f2a530474fec73e7d178ab62566e5
Rajakavitha1 <krajakavitha@gmail.com>
2020-12-29 16:32:29 +0530

    Update handler.go
    added the message to set name

--- x/nameservice/handler.go
+++ x/nameservice/handler.go
@@ -24,6 +24,15 @@ func NewHandler(keeper Keeper) sdk.Handler {
 	}
 }
 
+// Handle a message to set name
+func handleMsgSetName(ctx sdk.Context, keeper Keeper, msg MsgSetName) (*sdk.Result, error) {
+	if !msg.Owner.Equals(keeper.GetOwner(ctx, msg.Name)) { // Checks if the the msg sender is the same as the current owner
+		return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "Incorrect Owner") // If not, throw an error
+	}
+	keeper.SetName(ctx, msg.Name, msg.Value) // If so, set the name to the value specified in the msg.
+	return &sdk.Result{}, nil                // return
+}
+
 // Handle a message to buy name
 func handleMsgBuyName(ctx sdk.Context, keeper keeper.Keeper, msg types.MsgBuyName) (*sdk.Result, error) {
 	// Checks if the the bid price is greater than the price paid by the current owner
