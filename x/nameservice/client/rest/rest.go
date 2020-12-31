e6c18daabefc4a4d66dc8334a45513493f03ccce
Rajakavitha1 <krajakavitha@gmail.com>
2020-12-31 09:06:27 +0530

    Update rest.go
    added string to buyNameHandler

--- x/nameservice/client/rest/rest.go
+++ x/nameservice/client/rest/rest.go
@@ -9,7 +9,7 @@ import (
 // RegisterRoutes registers nameservice-related REST handlers to a router
 func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router) {
 	// this line is used by starport scaffolding
-	r.HandleFunc("/nameservice/whois", buyNameHandler(cliCtx)).Methods("POST")
+	r.HandleFunc("/nameservice/whois", buyNameHandler(cliCtx, string)).Methods("POST")
 	r.HandleFunc("/nameservice/whois", listWhoisHandler(cliCtx, "nameservice")).Methods("GET")
 	r.HandleFunc("/nameservice/whois/{key}", getWhoisHandler(cliCtx, "nameservice")).Methods("GET")
 	r.HandleFunc("/nameservice/whois/{key}/resolve", resolveNameHandler(cliCtx, "nameservice")).Methods("GET")
